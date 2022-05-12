6250545c4aec833501a3c26daa9bed8b3e88ec9c
HamadTria <102798449+HamadTria@users.noreply.github.com>
2022-05-13 00:56:50 +0200

    Update GUI.py

--- view/GUI.py
+++ view/GUI.py
@@ -5,6 +5,7 @@ import plotly.figure_factory as ff
 from dash import dcc
 from dash import dash_table
 
+#Multi-Value Dropdown (In use : Displot chart)
 def build_multi_value_dropdown_menu_distplot(menu_items):
     return dcc.Dropdown(
         id="multi_value_dropdown",
@@ -13,6 +14,7 @@ def build_multi_value_dropdown_menu_distplot(menu_items):
         multi=True
     )
 
+#Unique-Value Dropdown (In use : Animated bar chart)
 def build_dropdown_menu(menu_items):
     return dcc.Dropdown(
         id="dropdown",
@@ -21,20 +23,14 @@ def build_dropdown_menu(menu_items):
         clearable=False,
     )
 
+#Multi-Value Dropdown (In use : Scatter matrix)
 def build_dropdown_menu_multi():
     return dcc.Dropdown(
         id="dropdown6",
-        options=[{"label": x, "value": x} for x in ['Range','Altitude',
-        'harv','Year','Ntot1','Ntot','Mtot','oneacorn','VH','H','SH',
-        'tot_Germ','M_Germ','N_Germ','rate_Germ']],
-        value=['oneacorn','Ntot','Mtot'],
-        multi=True
-    )
-def build_dropdown_menu_multi_stations():
-    return dcc.Dropdown(
-        id="dropdown6",
-        options=[{"label": x, "value": x} for x in ['Josbaig','Le-Hourcq','Bager','Gabas','Artouste','Laveyron','Ibos','Gedre-Bas','Papillon','Peguere']],
-        value=['Josbaig'],
+        options=[{"label": x, "value": x} for x in ['Range', 'Altitude',
+                                                    'harv', 'Year', 'Ntot1', 'Ntot', 'Mtot', 'oneacorn', 'VH', 'H', 'SH',
+                                                    'tot_Germ', 'M_Germ', 'N_Germ', 'rate_Germ']],
+        value=['oneacorn', 'Ntot', 'Mtot'],
         multi=True
     )
 
@@ -54,18 +50,22 @@ def build_figure_distplot(df, labels):
 
 def build_figure_animated_bar_chart(df, attributes):
     x, y, t, z = attributes
-    fig = px.bar(df, x=z, y=x, animation_frame=t, animation_group=x, color=y, log_y=True)
+    fig = px.bar(df, x=z, y=x, animation_frame=t,
+                 animation_group=x, color=y, log_y=True)
     return fig
 
 def build_figure_scatter_matrix(df, dims):
     fig = px.scatter_matrix(df, dimensions=df[dims], color='Station')
     return fig
 
+
 def data_table(dataframe):
     return dash_table.DataTable(data=dataframe.to_dict('records'),
-                                columns=[{"name": i, "id": i} for i in dataframe.columns],
+                                columns=[{"name": i, "id": i}
+                                         for i in dataframe.columns],
                                 page_size=30,
                                 sort_action="native",
                                 sort_mode="multi",
-                                style_data={'whiteSpace': 'normal', 'height': 'auto'},
+                                style_data={'whiteSpace': 'normal',
+                                            'height': 'auto'},
                                 )
