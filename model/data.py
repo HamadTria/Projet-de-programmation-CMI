c5fb83fdd0204e4918dd1d0eb2519b5b34a3b175
HamadTria <102798449+HamadTria@users.noreply.github.com>
2022-05-13 00:56:16 +0200

    Update data.py

--- model/data.py
+++ model/data.py
@@ -1,19 +1,19 @@
 import pandas as pd
 
 df = pd.read_csv('model/Repro_IS.csv', sep=';')
-select_column = 'Valley'
-select_column2 = 'Year'
 
-def get_unique_values():
+
+def get_unique_values(select_column):
     return df[select_column].unique()
 
-def get_unique_values_displot(column):
-    return df[column].unique()
+# Retourne une liste de liste des Oneacorn (en supprimant les 'NaN' des valeurs) de chaque Station en fonction du Multi-Value Dropdown.
 
-def extract_df_distplot(dims):
-    return [df[(df['Station'] == Station) & (df['oneacorn'].notna())]['oneacorn'].to_numpy() for Station in dims]
 
-def extract_df_animated_bar_chart(value):
+def extract_df_distplot(multi_value_dropdown):
+    return [df[(df['Station'] == Station) & (df['oneacorn'].notna())]['oneacorn'].to_numpy() for Station in multi_value_dropdown]
+
+
+def extract_df_animated_bar_chart(value, select_column):
     mask = df[select_column] == value
     df_valley = df[mask]
     # attributes used to specify grouping (and view)
@@ -22,20 +22,23 @@ def extract_df_animated_bar_chart(value):
     t_att = 'Year'
     z_att = 'Station'
 
-    df_agreg = df_valley[[x_att, y_att, t_att, z_att]].sort_values(by=[t_att],ascending=True)
-    df_agreg = df_valley[[x_att, y_att, t_att, z_att]].groupby(by=[z_att, t_att]).sum()
+    df_agreg = df_valley[[x_att, y_att, t_att, z_att]
+                         ].sort_values(by=[t_att], ascending=True)
+    df_agreg = df_valley[[x_att, y_att, t_att, z_att]
+                         ].groupby(by=[z_att, t_att]).sum()
     df_agreg = df_agreg.reset_index()
 
     if value == 'Luz':
-        mask = df_agreg['Year'] != 2011 
+        mask = df_agreg['Year'] != 2011
         df_agreg = df_agreg[mask]
-    
+
     if value == 'Ossau':
         mask = df_agreg['Year'] != 2011
         df_agreg = df_agreg[mask]
         mask = df_agreg['Year'] != 2013
         df_agreg = df_agreg[mask]
     return df_agreg, (x_att, y_att, t_att, z_att)
+
 
 def extract_df_scatter_matrix():
     return df
