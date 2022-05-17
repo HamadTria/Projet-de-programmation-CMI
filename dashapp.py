384f00750856828eb923eac2c329563431f23dcf
Mine (Alexandre Leys) <35851329+Myuketsu@users.noreply.github.com>
2022-05-17 02:40:47 +0200

    Auto-refresh graph

--- dashapp.py
+++ dashapp.py
@@ -6,6 +6,8 @@ from dash.dependencies import Input, Output, State
 import model.sql_loader as sl
 import model.data as data
 
+from dash_extensions import Purify
+
 
 app = dash.Dash(__name__, suppress_callback_exceptions=True)
 
@@ -28,7 +30,14 @@ app.layout = html.Div([
 	[Input("url", "pathname")])
 def render_page_content(pathname):
 	if pathname == '/':
-		return [dcc.Graph(id='graph', figure=data.real_figure(con))]
+		return [dcc.Graph(id='graph', animate=False), 
+		dcc.Interval(id='auto_refresh', interval=50, n_intervals=0, max_intervals=10)]
+
+@app.callback(
+	Output("graph", "figure"),
+	[Input("auto_refresh", "n_intervals")])
+def refresh(nbr):
+	return data.call_figure(con, nbr)
 
 if __name__=='__main__':
 	app.run_server(debug=True)
