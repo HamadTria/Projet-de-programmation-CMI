df110be3c7fdd9dd3c9d2bb3a7dfe1331760d00e
Mine (Alexandre Leys) <35851329+Myuketsu@users.noreply.github.com>
2022-05-17 19:02:29 +0200

    Add selectable menu

--- dashapp.py
+++ dashapp.py
@@ -1,15 +1,11 @@
 import dash, sqlite3
-from dash import html
 from dash import dcc
 import dash_bootstrap_components as dbc
-from dash.dependencies import Input, Output, State
+from dash import Input, Output, html
 import model.sql_loader as sl
 import model.data as data
 
-from dash_extensions import Purify
-
-
-app = dash.Dash(__name__, suppress_callback_exceptions=True)
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP], suppress_callback_exceptions=True)
 
 con = sqlite3.connect('ABunchOfTrees.db', check_same_thread=False)
 cur = con.cursor()
@@ -20,8 +16,54 @@ con.commit()
 
 content = html.Div(id="page-content", children=[])
 
+list_group = dbc.ListGroup([
+                dbc.ListGroupItem([
+					html.Div([
+							html.H5("Ntot ~", className="mb-n1"),
+							html.Div([
+								html.Small("Data sanity : "),
+								html.Small("Clean !", className="text-success")
+							])
+						],
+						className="d-flex w-100 justify-content-between"
+					),
+					html.Hr(style={'margin-top':'1px', 'margin-bottom':'5px'}),
+					html.Small("Total quantity of acorns produced.", className="text-center")
+					], id='Ntot', action=True, active=True),
+
+				dbc.ListGroupItem([
+					html.Div([
+							html.H5("Oneacorn ~", className="mb-n1"),
+							html.Div([
+								html.Small("Data sanity : "),
+								html.Small("Clean !", className="text-success")
+							])
+						],
+						className="d-flex w-100 justify-content-between"
+					),
+					html.Hr(style={'margin-top':'1px', 'margin-bottom':'5px'}),
+					html.Small("Average mass of an acorn (g).", className="text-center")
+					], id='oneacorn', action=True),
+
+				dbc.ListGroupItem([
+					html.Div([
+							html.H5("Ntot1 ~", className="mb-n1"),
+							html.Div([
+								html.Small("Data sanity : "),
+								html.Small("Unfinished", className="text-danger")
+							])
+						],
+						className="d-flex w-100 justify-content-between"
+					),
+					html.Hr(style={'margin-top':'1px', 'margin-bottom':'5px'}),
+					html.Small("Total quantity of acorns produced without sprouted fruit and without deteriorated acorns.", className="d-flex align-items-center")
+					], id='Ntot1', action=True),
+
+				], horizontal=True, className="mb-0 w-50 p-1")
+
 app.layout = html.Div([
 	dcc.Location(id="url"),
+	list_group,
 	content
 ])
 
@@ -30,14 +72,33 @@ app.layout = html.Div([
 	[Input("url", "pathname")])
 def render_page_content(pathname):
 	if pathname == '/':
-		return [dcc.Graph(id='graph', animate=False), 
-		dcc.Interval(id='auto_refresh', interval=50, n_intervals=0, max_intervals=10)]
+		return [dcc.Graph(id='graph'), 
+		dcc.Interval(id='auto_refresh', interval=50, n_intervals=0, max_intervals=9)]
 
 @app.callback(
-	Output("graph", "figure"),
-	[Input("auto_refresh", "n_intervals")])
-def refresh(nbr):
-	return data.call_figure(con, nbr)
+	Output("graph", "figure"), Output("auto_refresh", "n_intervals"),
+	[Input("Ntot", "active"), Input("oneacorn", "active"), Input("Ntot1", "active"),
+	Input("auto_refresh", "n_intervals")])
+def refresh(nbr1, nbr2, nbr3, nbr_auto):
+	ctx = dash.callback_context
+	input_id = ctx.triggered[0]["prop_id"].split(".")[0]
+	call = 'Ntot' if nbr1 else 'oneacorn' if nbr2 else 'Ntot1' if nbr3 else 'Ntot'
+	if input_id == 'auto_refresh':
+		return data.call_figure(con, call, nbr_auto), nbr_auto
+	return data.call_figure(con, call, 0), 0
+
+
+@app.callback(
+    Output("Ntot", "active"), Output("oneacorn", "active"), Output("Ntot1", "active"),
+    [Input("Ntot", "n_clicks"), Input("oneacorn", "n_clicks"), Input("Ntot1", "n_clicks")])
+def a(nbr1, nbr2, nbr3):
+	ctx = dash.callback_context
+	input_id = ctx.triggered[0]["prop_id"].split(".")[0]
+	if input_id == 'oneacorn':
+		return False, True, False
+	elif input_id == 'Ntot1':
+		return False, False, True
+	return True, False, False
 
 if __name__=='__main__':
 	app.run_server(debug=True)
--- model/data.py
+++ model/data.py
@@ -2,16 +2,16 @@ import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 
-def call_figure(con, index=0):
-    df = pd.read_sql('SELECT oneacorn, Year FROM harvest;', con)
-    df = df.groupby(by=['Year'])['oneacorn'].agg(['max', 'min', ('mean oneacorn', 'mean')])
-    df['max'] = df['max'] - df['mean oneacorn']
-    df['min'] = df['mean oneacorn'] - df['min']
+def call_figure(con, column, index=0):
+    df = pd.read_sql(f'SELECT {column}, Year FROM harvest;', con)
+    df = df.groupby(by=['Year'])[column].agg(['max', 'min', 'mean'])
+    df['max'] = df['max'] - df['mean']
+    df['min'] = df['mean'] - df['min']
 
     fig = go.Figure(
         data=[go.Scatter(
                     x = (df.index).tolist()[:index+1] if index != 0 else [(df.index).tolist()[index]],
-                    y = df['mean oneacorn'].iloc[:index+1] if index != 0 else [df['mean oneacorn'].iloc[index]],
+                    y = df['mean'].iloc[:index+1] if index != 0 else [df['mean'].iloc[index]],
                     error_y = dict(
                         type = 'data',
                         symmetric = False,
@@ -19,6 +19,6 @@ def call_figure(con, index=0):
                         arrayminus = df['min'].iloc[:index+1] if index != 0 else [df['min'].iloc[index]]))],
         layout = go.Layout(
             xaxis = dict(range=[2010,2021], autorange=False, zeroline=False, title='Year'),
-            yaxis = dict(range=[-1,7], autorange=False, zeroline=False, title='Mean oneacorn'),
+            yaxis = dict(range=[df['min'].min() - df['mean'].max(), df['max'].max() + 2*df['mean'].max()], autorange=False, zeroline=False, title=f'Mean {column}'),
             title_text = "OMG!", hovermode="closest"))
     return fig
